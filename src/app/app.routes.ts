d1b56c841b9e754494134d126c324b452c629be3
iyksmir <magnezia98@mail.ru>
2025-09-06 18:27:54 +0300

    task-003: Добавлена вторая страница с последними рецептами. Стили

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
-import { Welcome } from '../pages/index';
+import { IndexPage } from '../pages/index';
+import { MainPage } from '../pages/main';
 
 export const routes: Routes = [
   {
       path: '',
-      component: Welcome,
-  }
+      component: IndexPage,
+      title: "Хранилище рецептов"
+  },
+  {
+    path: 'main',
+    component: MainPage,
+    title: "Последние ваши рецепты",
+  },
 ];
---
+++ src/pages/main/index.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'main-page',
+    templateUrl: './index.html',
+    styleUrl: './index.css',
+})
+
+export class MainPage {};
