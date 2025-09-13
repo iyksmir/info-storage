af9ec263bb931df59da80b51a461993e0569efe1
iyksmir <magnezia98@mail.ru>
2025-09-13 23:47:40 +0300

    task-005: Адаптив. Вынесение превью карточек в компонент

---
+++ src/pages/main/components/recipe-preview-card/index.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'recipe-preview-card',
+    templateUrl: './index.html',
+    styleUrl: './index.css',
+})
+
+export class RecipePreviewCard {}
